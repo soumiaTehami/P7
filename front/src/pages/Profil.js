58461e58d8dbc12a43ab041ffc9772fcb3d27d95
tehami soumia <sou_mia_46@hotmail.fr>
2022-08-18 10:30:17 +0200

    page profil

--- front/src/pages/Profil.js
+++ front/src/pages/Profil.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
+import UpdateProfil from "../components/Profil/UpdateProfil";
 
 
 const Profil = () => {
   return (
     <div className="profil-page">
-     
+     <UpdateProfil />
         <div className="log-container">
         <Navbar />
           
-        salut
+        
           </div>
       
     </div>
